cda335b333f9ba6c3f610fb02622c468837bc3c4
Priste94 <pristestore@gmail.com>
2022-05-17 15:39:15 +0200

    prova

--- src/main/java/com/app/NFT/config/RestServiceConfig.java
+++ src/main/java/com/app/NFT/config/RestServiceConfig.java
@@ -13,6 +13,7 @@ public class RestServiceConfig {
 	@Value("${allowed.origin}")
 	private String allowedOrigin;
 	
+	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
